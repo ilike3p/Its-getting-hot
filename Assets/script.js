6ea0894d2e799100245d0ea2cf26b050e59bf39e
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-26 10:26:51 -0800

    update js

--- Assets/script.js
+++ Assets/script.js
@@ -7,3 +7,16 @@ var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+
+//set an unique API to a variable
+const apiKey = "d85561a519f7331fb2a3d44c69713388"
+
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
